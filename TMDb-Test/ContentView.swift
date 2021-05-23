fc6474375ba0cd69f71ff65cff994ef181ecb855
viktor <clint@ua.fm>
2021-05-23 12:28:38 +0300

    Sign in screen

--- TMDb-Test/ContentView.swift
+++ TMDb-Test/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var login = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            VStack {
+                
+                
+                Section {
+                    TextField("Login", text: $login)
+                        //                            .background(Color.yellow)
+                        .padding(.horizontal, 20)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal, 20)
+                    
+                    
+                }
+                .padding(.horizontal, 20)
+                .padding(.top, 10)
+                
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Sign In")
+                        .frame(width: 210, height: 50, alignment: .center)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(15)
+                })
+                .padding(.top, 10)
+                
+                Spacer()
+            }
+            .navigationTitle("Sign In")
+        }
     }
 }
 
