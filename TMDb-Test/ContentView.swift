4487f8499946deea8efca56d6f784b6bdc154b40
viktor <clint@ua.fm>
2021-05-23 11:44:28 +0300

    Initial Commit

---
+++ TMDb-Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TMDb-Test
+//
+//  Created by Віктор Бережницький on 23.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TMDb-Test/TMDb_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDb_TestApp.swift
+//  TMDb-Test
+//
+//  Created by Віктор Бережницький on 23.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct TMDb_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
