dd6466c96a64a44ca7ceb36fb0af6b058b3df4c8
viktor <clint@ua.fm>
2021-05-24 18:42:27 +0300

    Added Main Screen

--- TMDb-Test/PopularMoviesResponceModel.swift
+++ TMDb-Test/PopularMoviesResponceModel.swift
@@ -33,30 +33,3 @@ struct Movie: Codable {
     let vote_average: Double
     let vote_count: Int
 }
-
-//// MARK: - PopularMoviesResponceModel
-//struct PopularMoviesResponceModel: Codable {
-//    let page: Int?
-//    let results: [Movie]
-//    let totalPages: Int?
-//    let totalResults: Int?
-//}
-//
-//
-//// MARK: - Result
-//struct Movie: Codable {
-//    let adult: Bool?
-//    let backdrop_path: String?
-//    let genre_ids: [Int]?
-//    let id: Int?
-//    let original_language: String?
-//    let original_title: String?
-//    let overview: String?
-//    let popularity: Double?
-//    let poster_path: String?
-//    let release_date: String?
-//    let title: String?
-//    let video: Bool?
-//    let vote_average: Double?
-//    let vote_count: Int?
-//}
